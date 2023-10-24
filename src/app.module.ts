72fdfcd719416cf438afda3c4ef9285715dff037
HUEN_OH <heons921@gmail.com>
2023-10-24 19:45:06 -0400

    use nest-winston

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggerModule } from './logger/logger.module';
+// import { LoggerModule } from './logger/logger.module';
+
+import * as winston from 'winston';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike('MyApp', {
+              prettyPrint: true,
+            }),
+          ),
+        }),
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,21 +1,29 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { MyLogger3 } from './logger/logger.service';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 // import { MyLogger } from './MyLogger';
 // import { MyLogger2 } from './MyLogger2';
+// import { MyLogger3 } from './logger/logger.service';
+import { Logger as WinstonLogger } from 'winston';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 
 @Injectable()
 export class AppService {
   // private readonly logger: Logger = new Logger(AppService.name);
   // private readonly logger: MyLogger = new MyLogger();
   // private readonly logger: MyLogger2 = new MyLogger2();
-  constructor(private myLogger: MyLogger3) {}
+  // constructor(private myLogger: MyLogger3) {}
+  constructor(@Inject(WINSTON_MODULE_PROVIDER) private logger: WinstonLogger) {}
 
   getHello(): string {
-    this.myLogger.error('level: error');
-    this.myLogger.warn('level: warn');
-    this.myLogger.log('level: log');
-    this.myLogger.verbose('level: verbose');
-    this.myLogger.debug('level: debug');
+    const mockDto = {
+      name: 'Name',
+      email: 'email@example.com',
+      password: 'passw;ord',
+    };
+    this.logger.error('level: error', mockDto);
+    this.logger.warn('level: warn', mockDto);
+    this.logger.info('level: info', mockDto);
+    this.logger.verbose('level: verbose', mockDto);
+    this.logger.debug('level: debug', mockDto);
 
     return 'Hello World!';
   }
