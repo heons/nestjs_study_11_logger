e70ed1196b74d88f83a733e95bea22fab99632c5
HUEN_OH <heons921@gmail.com>
2023-10-24 19:24:36 -0400

    use MyLogger from LoggerService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { MyLogger } from './MyLogger';
 
 @Injectable()
 export class AppService {
-  private readonly logger: Logger = new Logger(AppService.name);
+  // private readonly logger: Logger = new Logger(AppService.name);
+  private readonly logger: MyLogger = new MyLogger();
 
   getHello(): string {
     this.logger.error('level: error');
