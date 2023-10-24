79e28c61bcfa8c8c57e69bf534b9d55e2cf33adc
HUEN_OH <heons921@gmail.com>
2023-10-24 19:28:09 -0400

    user MyLogger2 from ConsoleLogger

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { MyLogger } from './MyLogger';
+// import { MyLogger } from './MyLogger';
+import { MyLogger2 } from './MyLogger2';
 
 @Injectable()
 export class AppService {
   // private readonly logger: Logger = new Logger(AppService.name);
-  private readonly logger: MyLogger = new MyLogger();
+  // private readonly logger: MyLogger = new MyLogger();
+  private readonly logger: MyLogger2 = new MyLogger2();
 
   getHello(): string {
     this.logger.error('level: error');
