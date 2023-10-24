f0c1396aa78c906f2f1973ebc71f2e86f5f2e731
HUEN_OH <heons921@gmail.com>
2023-10-24 19:36:23 -0400

    use MyLogger3 in global

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MyLogger3 } from './logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -8,6 +9,7 @@ async function bootstrap() {
         ? ['error', 'warn', 'log']
         : ['error', 'warn', 'log', 'verbose', 'debug'],
   });
+  app.useLogger(app.get(MyLogger3));
   await app.listen(3000);
 }
 bootstrap();
